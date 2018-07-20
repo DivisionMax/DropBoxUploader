ec82076b7d7e7c56411a4ea04ed54de9f95fca66
gavinw <gavinwiener@gmail.com>
2018-07-20 09:45:01 +0700

    Use a yaml file. Do not include access tokens

--- upload_to_db.py
+++ upload_to_db.py
@@ -1,16 +1,30 @@
-import dropbox
 import os
+import logging
+import yaml
+import dropbox
 from dropbox.files import WriteMode
 
-token = 'lPMo4FcCrwAAAAAAAAABqsVaWnzOqyMlWJmqSWowFiWL01Ci2tQYYoEjY3ogPeaf'
-upload_folder = 'Pi Frames V1'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config = None
+
+with open("config.yml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        logger.error(exc)
+
+logger.info(config)
+
+token = config['token']
+upload_folder = config['dbx_folder']
 
 dbx = dropbox.Dropbox(token)
 
 def backup(filepath):
     with open(filepath, 'rb') as f:
-        # We use WriteMode=overwrite to make sure that the settings in the file
-        # are changed on upload
+        # Upload path must include a forward-slash at the start
         upload_path = os.path.join(filepath) 
         print("Uploading " + filepath + " to Dropbox as " + upload_path + "...")
         dbx.files_upload(f.read(), '/test_folder/' + upload_path, mode=WriteMode('overwrite'))
